c13bcd116d6bc3041e408f5c6db22cbf430a4646
Gaurav <gaurav.123337@gmail.com>
2021-03-21 22:47:22 +0530

    deployment changes

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import App from './App';
 import ApolloClient from 'apollo-client';
-import {InMemoryCache} from 'apollo-cache-inmemory';
-import {createHttpLink} from 'apollo-link-http';
-import {ApolloProvider} from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  // uri: 'http://localhost:5000',
+  uri: "https://protected-lake-85284.herokuapp.com/"
 });
 
 const authLink = setContext(() => {
