d3e3b046f8fcbbb4f63307a328acde876d2c88cc
Gaurav <gaurav.123337@gmail.com>
2021-03-21 22:59:08 +0530

    Update apollo provider

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,6 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  // uri: 'http://localhost:5000',
   uri: "https://protected-lake-85284.herokuapp.com/"
 });
 
